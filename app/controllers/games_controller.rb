bd0770a245f38c4c4c63afeebd9e394544df4a94
Jason Fu <jasonguangfu@gmail.com>
2019-03-25 17:06:21 +0800

    done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,53 @@
+require 'time'
+
+class GamesController < ApplicationController
+  def new
+    @grid = []
+    alphabet = ('A'..'Z').to_a
+    10.times { |_i| @grid << alphabet.sample }
+    @start_time = Time.now
+  end
+
+  def score
+    end_time = Time.now
+    @score = run_game(params[:attempt], params[:grid], params[:start_time], end_time)
+  end
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def compute_score(attempt, time_taken)
+    time_taken > 60.0 ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+    result = { time: end_time - Time.parse(start_time) }
+
+    score_and_message = score_and_message(attempt, grid, result[:time])
+    result[:score] = score_and_message.first
+    result[:message] = score_and_message.last
+
+    result
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+
+  def score_and_message(attempt, grid, time)
+    if included?(attempt.upcase, grid)
+      if english_word?(attempt)
+        score = compute_score(attempt, time)
+        [score, "well done"]
+      else
+        [0, "not an english word"]
+      end
+    else
+      [0, "not in the grid"]
+    end
+  end
+end
+
